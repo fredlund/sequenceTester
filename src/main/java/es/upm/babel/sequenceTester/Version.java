13b2c3b8ace172c91fca0939910ef4da78b6c1ea
Lars-Ake Fredlund <fredlund1@gmail.com>
2020-04-17 16:19:22 +0200

    added version to the library

---
+++ src/main/java/es/upm/babel/sequenceTester/Version.java
@@ -0,0 +1,37 @@
+package es.upm.babel.sequenceTester;
+
+/**
+ *  Provides information about the library version.
+ *  Library version numbers use the scheme Major.Minor.Patch,
+ *  where normally a change in the Major number signals an incompatible
+ * API changes, a change in the Minor number signals additions to the API
+ * while preserving compatibility for older programs, and changes in
+ * the Patch number corrects
+ * errors in the library while not changing the API (except fixing the bug).
+ *
+ */
+public class Version {
+    /**
+     * Returns the major version number.
+     * @return the major version number.
+     */
+    public static int major() {
+	return 1;
+    }
+
+    /**
+     * Returns the minor version number.
+     * @return the minor version number.
+     */
+    public static int minor() {
+	return 0;
+    }
+    
+    /**
+     * Returns the patchlevel revision number.
+     * @return the patchlevel revision number.
+     */
+    public static int patchlevel() {
+	return 0;
+    }
+}
